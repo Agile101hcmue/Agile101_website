b52c07cb7c356895d20c039a00617f5c87808333
Agile101 <126162299+Agile101hcmue@users.noreply.github.com>
2023-03-20 16:31:21 +0700

    OS-03: added footer for homepage

---
+++ footer.php
@@ -0,0 +1,37 @@
+<footer>
+<div class="container">
+  <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+    <div class="col mb-3">
+      <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <p class="text-muted"><strong>Agile101 © 2023</strong></p>
+      <p class="text-muted"><i class="fa fa-code" aria-hidden="true"></i>&nbsp;github@TrHgTung (Hoàng Tùng)</p>
+    </div>
+
+    <div class="col mb-3">
+
+    </div>
+
+    <div class="col mb-3">
+      <h5>Liên hệ với chúng tôi</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="https://www.facebook.com/nguyentuanhung12345" class="nav-link p-0 text-muted">Facebook</a></li>
+        <li class="nav-item mb-2"><a href="mailto:tungng14@gmail.com" class="nav-link p-0 text-muted">Gmail (Feedback)</a></li>
+        <li class="nav-item mb-2"><a href="https://www.github.com/TrHgTung" class="nav-link p-0 text-muted">Github</a></li>
+        <li class="nav-item mb-2"><a href="./components/privacy.php" class="nav-link p-0 text-muted">Privacy Policy</a></li>
+      </ul>
+    </div>
+
+    <div class="col mb-3">
+      <h5>Điều hướng nhanh</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="./" class="nav-link p-0 text-muted">Trang chủ</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Sản phẩm</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Khuyến mãi</a></li>
+        <li class="nav-item mb-2"><a href="./components/cart/cart.php" class="nav-link p-0 text-muted">Giỏ hàng</a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
+</footer>
