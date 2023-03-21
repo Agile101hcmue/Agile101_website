64bd0b537c148aa936bc56495f125d6358a21b7c
Agile101 <126162299+Agile101hcmue@users.noreply.github.com>
2023-03-21 13:29:10 +0700

    OS-08: added post method from HTML form to PHP
    30min (thanh)

--- components/signup.php
+++ components/signup.php
@@ -1,4 +1,32 @@
+<?php
+    session_start();
 
+    include("connectdb.php");
+    include("functiondb.php");
+
+
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $user_name = $_POST['user_name'];
+        $password = $_POST['password'];
+        $gender = $_POST['gender'];
+        $birthyear = $_POST['birthyear'];
+        $location = $_POST['location'];
+
+        if(!empty($user_name) && !empty($password) && !is_numeric($user_name)){
+            //
+            $user_id = random_num(20);
+            $query = "insert into users (user_id,user_name,password,gender,birthyear,location) values ('$user_id','$user_name','$password','$gender','$birthyear','$location')";
+
+            mysqli_query($con, $query);
+
+            header("Location: login.php");
+            die;
+        }else{
+            echo "Please enter some value";
+        }
+    }
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
