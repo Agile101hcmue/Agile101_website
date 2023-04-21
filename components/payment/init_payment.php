1b61235d3448425db20d27518c3d223da2a18a3e
Agile101 <126162299+Agile101hcmue@users.noreply.github.com>
2023-04-21 19:16:33 +0700

    OS-21: Init MoMo Payment Function

---
+++ components/payment/init_payment.php
@@ -0,0 +1,202 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+
+
+function execPostRequest($url, $data)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data))
+    );
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    //execute post
+    $result = curl_exec($ch);
+    //close connection
+    curl_close($ch);
+    return $result;
+}
+
+
+$endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
+
+
+$partnerCode = 'MOMOBKUN20180529';
+$accessKey = 'klm05TvNBzhg7h7j';
+$secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
+$orderInfo = "Thanh toán qua MoMo";
+$amount = "10000";
+$orderId = time() ."";
+$redirectUrl = "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b";
+$ipnUrl = "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b";
+$extraData = "";
+
+
+if (!empty($_POST)) {
+    $partnerCode = $_POST["partnerCode"];
+    $accessKey = $_POST["accessKey"];
+    $serectkey = $_POST["secretKey"];
+    $orderId = $_POST["orderId"]; // Mã đơn hàng
+    $orderInfo = $_POST["orderInfo"];
+    $amount = $_POST["amount"];
+    $ipnUrl = $_POST["ipnUrl"];
+    $redirectUrl = $_POST["redirectUrl"];
+    $extraData = $_POST["extraData"];
+
+    $requestId = time() . "";
+    $requestType = "captureWallet";
+    $extraData = ($_POST["extraData"] ? $_POST["extraData"] : "");
+    //before sign HMAC SHA256 signature
+    $rawHash = "accessKey=" . $accessKey . "&amount=" . $amount . "&extraData=" . $extraData . "&ipnUrl=" . $ipnUrl . "&orderId=" . $orderId . "&orderInfo=" . $orderInfo . "&partnerCode=" . $partnerCode . "&redirectUrl=" . $redirectUrl . "&requestId=" . $requestId . "&requestType=" . $requestType;
+    $signature = hash_hmac("sha256", $rawHash, $serectkey);
+    $data = array('partnerCode' => $partnerCode,
+        'partnerName' => "Test",
+        "storeId" => "MomoTestStore",
+        'requestId' => $requestId,
+        'amount' => $amount,
+        'orderId' => $orderId,
+        'orderInfo' => $orderInfo,
+        'redirectUrl' => $redirectUrl,
+        'ipnUrl' => $ipnUrl,
+        'lang' => 'vi',
+        'extraData' => $extraData,
+        'requestType' => $requestType,
+        'signature' => $signature);
+    $result = execPostRequest($endpoint, json_encode($data));
+    $jsonResult = json_decode($result, true);  // decode json
+
+    //Just a example, please check more in there
+
+    header('Location: ' . $jsonResult['payUrl']);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>MoMo Sandbox</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css"/>
+    <link rel="stylesheet"
+          href="./statics/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css"/>
+    <!-- CSS -->
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Initial payment/Khởi tạo thanh toán</h3>
+                </div>
+                <div class="panel-body">
+                    <form class="" method="POST" target="_blank" enctype="application/x-www-form-urlencoded"
+                          action="init_payment.php">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="fxRate" class="col-form-label">PartnerCode</label>
+                                    <div class='input-group date' id='fxRate'>
+                                        <input type='text' name="partnerCode" value="<?php echo $partnerCode; ?>"
+                                               class="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="fxRate" class="col-form-label">AccessKey</label>
+                                    <div class='input-group date' id='fxRate'>
+                                        <input type='text' name="accessKey" value="<?php echo $accessKey;?>"
+                                               class="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="fxRate" class="col-form-label">SecretKey</label>
+                                    <div class='input-group date' id='fxRate'>
+                                        <input type='text' name="secretKey" value="<?php echo $secretKey; ?>"
+                                               class="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="fxRate" class="col-form-label">OrderId</label>
+                                    <div class='input-group date' id='fxRate'>
+                                        <input type='text' name="orderId" value="<?php echo $orderId; ?>"
+                                               class="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="fxRate" class="col-form-label">ExtraData</label>
+                                    <div class='input-group date' id='fxRate'>
+                                        <input type='text' type="text" name="extraData" value="<?php echo $extraData?>"
+                                               class="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="fxRate" class="col-form-label">OrderInfo</label>
+                                    <div class='input-group date' id='fxRate'>
+                                        <input type='text' name="orderInfo" value="<?php echo $orderInfo; ?>"
+                                               class="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="fxRate" class="col-form-label">Amount</label>
+                                    <div class='input-group date' id='fxRate'>
+                                        <input type='text' name="amount" value="<?php echo $amount; ?>" class="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="fxRate" class="col-form-label">IpnUrl</label>
+                                    <div class='input-group date' id='fxRate'>
+                                        <input type='text' name="ipnUrl" value="<?php echo $ipnUrl; ?>"
+                                               class="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="fxRate" class="col-form-label">RedirectUrl</label>
+                                    <div class='input-group date' id='fxRate'>
+                                        <input type='text' name="redirectUrl" value="<?php echo $redirectUrl; ?>"
+                                               class="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <p>
+                        <div style="margin-top: 1em;">
+                            <button type="submit" class="btn btn-primary btn-block">Start MoMo payment....</button>
+                        </div>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<script type="text/javascript" src="./statics/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="./statics/moment/min/moment.min.js"></script>
+</html>
---
+++ components/payment/ipn_momo.php
@@ -0,0 +1,57 @@
+<?php
+header("content-type: application/json; charset=UTF-8");
+http_response_code(200); //200 - Everything will be 200 Oke
+if (!empty($_POST)) {
+    $response = array();
+    try {
+        $partnerCode = $_POST["partnerCode"];
+		$orderId = $_POST["orderId"];
+		$requestId = $_POST["requestId"];
+		$amount = $_POST["amount"];	
+		$orderInfo = $_POST["orderInfo"];
+		$orderType = $_POST["orderType"];
+		$transId = $_POST["transId"];
+		$resultCode = $_POST["resultCode"];
+		$message = $_POST["message"];
+		$payType = $_POST["payType"];
+		$responseTime = $_POST["responseTime"];
+		$extraData = $_POST["extraData"];
+		$m2signature = $_POST["signature"]; //MoMo signature
+		
+
+		//Checksum
+		$rawHash = "accessKey=" . $accessKey . "&amount=" . $amount . "&extraData=" . $extraData . "&message=" . $message . "&orderId=" . $orderId . "&orderInfo=" . $orderInfo .
+			"&orderType=" . $orderType . "&partnerCode=" . $partnerCode . "&payType=" . $payType . "&requestId=" . $requestId . "&responseTime=" . $responseTime .
+			"&resultCode=" . $resultCode . "&transId=" . $transId;
+
+        $partnerSignature = hash_hmac("sha256", $rawHash, 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa');
+
+        if ($m2signature == $partnerSignature) {
+            if ($errorCode == '0') {
+                $result = '<div class="alert alert-success">Capture Payment Success</div>';
+            } else {
+                $result = '<div class="alert alert-danger">' . $message . '</div>';
+            }
+        } else {
+            $result = '<div class="alert alert-danger">This transaction could be hacked, please check your signature and returned signature</div>';
+        }
+
+    } catch (Exception $e) {
+        echo $response['message'] = $e;
+    }
+
+    $debugger = array();
+    $debugger['rawData'] = $rawHash;
+    $debugger['momoSignature'] = $m2signature;
+    $debugger['partnerSignature'] = $partnerSignature;
+
+    if ($m2signature == $partnerSignature) {
+        $response['message'] = "Received payment result success";
+    } else {
+        $response['message'] = "ERROR! Fail checksum";
+    }
+    $response['debugger'] = $debugger;
+    echo json_encode($response);
+}
+
+?>
